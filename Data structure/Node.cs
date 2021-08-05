0c71e6664303e2ee469fc327d6e264fcd6956996
dnyana <dnyaneshgawade903@gmail.com>
2021-08-05 11:28:40 +0530

    [Dnyanesh] add :Add element into Linkedlist

---
+++ Data structure/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_structure
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
